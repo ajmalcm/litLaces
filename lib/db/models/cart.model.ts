f6f22f22c10aee1d47b8af47341659f716f75202
ajmalcm <ajmalcm22@gmail.com>
2025-07-28 01:27:09 +0530

    cartModel added

---
+++ lib/db/models/cart.model.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+interface CartItem extends Document {
+  user: Schema.Types.ObjectId;
+  cartItems: Array<{
+    product: Schema.Types.ObjectId;
+    quantity: number;
+    size: string;
+    price: number;
+    image: {
+      public_id: string;
+      url: string;
+    };
+  }>;
+}
+
+
+const CartSchema = new Schema<CartItem>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true ,unique: true },
+    cartItems: [
+      {
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: "Sneaker",
+          required: true,
+        },
+        quantity: { type: Number, required: true, default: 1 },
+        size: { type: String, required: true },
+        price: { type: Number, required: true },
+        image: {
+          public_id: { type: String, required: true },
+          url: { type: String, required: true },
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Cart || mongoose.model<CartItem>("Cart",CartSchema);
