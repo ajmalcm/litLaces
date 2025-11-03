d7c9de62e0abc5cc11ea0c3aa1d2d9dedc418a6b
ajmalcm <ajmalcm22@gmail.com>
2025-11-03 20:59:32 +0530

    modal ui comps form almost done

--- app/(admin)/admin/appearence/page.tsx
+++ app/(admin)/admin/appearence/page.tsx
@@ -1,18 +1,26 @@
 "use client"
 
+import UpdateCompsModal from "@/components/UpdateCompsModal";
 import { Edit } from "lucide-react";
 import React,{useState} from "react";
 
 
 const page = () => {
 
-  const fileType="img"; //will be vieo or audio
+  const fileType="img"; //will be video or audio
   const [updateBannerModal,setUpdateBannerModal]=useState(false);
 
   return (
 
     <div className="flex-1 flex max-h-fit flex-col md:max-h-[110vh]">
-       <p className="fixed md:top-0 top-[11vh] left-0 md:left-[14vw] z-20 font-bold text-xl backdrop-blur-md bg-white/20 border border-white/30 rounded-xl p-4 shadow-lg rounded-l-none md:rounded-bl-lg"><Edit/></p>
+       {
+        updateBannerModal && (
+          <div className="fixed top-0 left-0 w-full max-h-full z-30 overflow-auto">
+            <UpdateCompsModal setUpdateBannerModal={setUpdateBannerModal}/>
+            </div>
+       )}
+       <p className="fixed md:top-0 top-[11vh] left-0 md:left-[14vw] z-20 font-bold text-xl backdrop-blur-md
+        bg-white/20 border border-white/30 rounded-xl p-4 shadow-lg rounded-l-none md:rounded-bl-lg" onClick={()=>setUpdateBannerModal(true)}><Edit/></p>
       <div className="w-full flex flex-col md:flex-row gap-1 h-fit">
         <div className="flex-[0.7] border-[1px] border-color-white rounded-md relative">
           {
@@ -26,7 +34,7 @@ const page = () => {
         <div className="flex-[0.3] border-[1px] border-color-white rounded-md relative">
           {
             fileType==="img"?
-            <img src="/assets/phonegif.gif" alt="phoneGif" height={"60vh"} className="object-cover w-full h-full"/>:
+            <img src="/assets/phoneGif.gif" alt="phoneGif" height={"60vh"} className="object-cover w-full h-full"/>:
             <video/>
           }
           <p className="absolute top-1/2 left-1/2 text-3xl -translate-x-1/2 -translate-y-1/ font-extrabold">LIT LACES</p>
@@ -44,7 +52,6 @@ const page = () => {
             <img src="/assets/all.jpg" alt="all" height={"30vh"} className="object-cover w-100 h-100"/>
         </div>
       </div>
-       
     </div>
   );
 };
--- components/UpdateCompsModal.tsx
+++ components/UpdateCompsModal.tsx
@@ -1,17 +1,44 @@
-import { Cross } from "lucide-react";
-import React from "react";
+"use client"
 
-const UpdateCompsModal = () => {
+import { Cross } from "lucide-react";
+import React, { useState } from "react";
+
+const UpdateCompsModal = ({setUpdateBannerModal}:{setUpdateBannerModal:Function}) => {
+
+  const [bannerData,setBannerData]=useState({heroL:"/assets/bbgif.gif",heroSM:"/assets/phoneGif.gif",banner1:"/assets/men.jpg",banner2:"/assets/women.jpg",banner3:"/assets/all.jpg"});
+  const {heroL,heroSM,banner1,banner2,banner3}=bannerData;
+
+
+const closeModal=()=>{
+  setUpdateBannerModal(false);
+}
+
   return (
-    <div className="p-2 md:p-4 flex justify-center items-center flex-1 w-full h-full mx-auto relative bg-gray-900 rounded-md">
-        <Cross className="absolute top-4 right-4 cursor-pointer text-white" size={24} />
-      <h2 className="text-2xl textwhite">Update Banners</h2>
-      <form>
+    <div className="p-2 md:p-4 flex flex-col justify-center items-center h-full mx-auto relative backdrop-blur-md overflow-x-scroll
+        bg-white/20 border border-white/30 rounded-md">
+        <Cross className="absolute top-4 right-4 cursor-pointer text-white rotate-45" size={24} onClick={closeModal}/>
+      <h2 className="text-2xl textwhite mb-3">Update Banners</h2>
+      <form className="max-w-full md:max-w-[70%] ">
         <div>
           <label
             htmlFor="heroLarge"
-            className="block text-gray-300 font-medium"
+            className="inline-flex justify-center items-center px-5 py-2.5 rounded-xl bg-gradient-to-r from-blue-600 to-purple-600 
+               text-white font-semibold shadow-lg cursor-pointer transition-transform 
+               hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-blue-400 w-full gap-2"
           >
+            <svg
+                className="w-5 h-5 mr-2"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 16v1a2 2 0 002 2h12a2 2 0 002-2v-1M12 12v9m0-9l-3 3m3-3l3 3m0-9a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+              </svg>
             Hero Large
           </label>
           <input
@@ -19,19 +46,33 @@ const UpdateCompsModal = () => {
             accept="image/*,video/*"
             multiple={false}
             id="heroLarge"
-            className="w-full mt-1 p-3 rounded-lg bg-gray-800 border border-gray-700
-               text-gray-200 focus:ring-2 focus:ring-gray-600 focus:outline-none mb-4"
+            className="hidden"
           />
           {/* preview of the selected sile*/}
           <div>
-            <img src="" alt="heroLargePreview" className="w-full h-auto mb-4" />
+            <img src={heroL} alt="heroLargePreview" className="w-full h-auto mb-4" />
           </div>
         </div>
         <div>
           <label
             htmlFor="heroMobile"
-            className="block text-gray-300 font-medium"
+            className="inline-flex justify-center items-center px-5 py-2.5 rounded-xl bg-gradient-to-r from-blue-600 to-purple-600 
+               text-white font-semibold shadow-lg cursor-pointer transition-transform 
+               hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-blue-400 w-full gap-2"
           >
+            <svg
+                className="w-5 h-5 mr-2"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 16v1a2 2 0 002 2h12a2 2 0 002-2v-1M12 12v9m0-9l-3 3m3-3l3 3m0-9a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+              </svg>
             Hero Mobile
           </label>
           <input
@@ -39,13 +80,12 @@ const UpdateCompsModal = () => {
             accept="image/*,video/*"
             multiple={false}
             id="heroMobile"
-            className="w-full mt-1 p-3 rounded-lg bg-gray-800 border border-gray-700
-               text-gray-200 focus:ring-2 focus:ring-gray-600 focus:outline-none mb-4"
+            className="hidden"
           />
           {/* preview of the selected sile*/}
           <div>
             <img
-              src=""
+              src={heroSM}
               alt="heroMobilePreview"
               className="w-full h-auto mb-4"
             />
@@ -53,30 +93,45 @@ const UpdateCompsModal = () => {
         </div>
 
         <div>
-          <label htmlFor="banners3" className="block text-gray-300 font-medium">
-            banners 3 images
-          </label>
+          <label htmlFor="banners3" className="inline-flex justify-center items-center px-5 py-2.5 rounded-xl bg-gradient-to-r from-blue-600 to-purple-600 
+               text-white font-semibold shadow-lg cursor-pointer transition-transform 
+               hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-blue-400 w-full gap-2"
+          >
+            <svg
+                className="w-5 h-5 mr-2"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 16v1a2 2 0 002 2h12a2 2 0 002-2v-1M12 12v9m0-9l-3 3m3-3l3 3m0-9a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+              </svg>
+              Mini Banners 
+              </label>
 
           <input
             type="file"
             accept="image/*"
             multiple
             id="banners3"
-            className="w-full mt-1 p-3 rounded-lg bg-gray-800 border border-gray-700
-               text-gray-200 focus:ring-2 focus:ring-gray-600 focus:outline-none mb-4"
+            className="hidden"
           />
 
           {/* preview of the selected files */}
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-            <img src="" alt="banner1Preview" className="w-full h-auto mb-4" />
-            <img src="" alt="banner2Preview" className="w-full h-auto mb-4" />
-            <img src="" alt="banner3Preview" className="w-full h-auto mb-4" />
+            <img src={banner1} alt="banner1Preview" className="w-full h-auto mb-4" />
+            <img src={banner2} alt="banner2Preview" className="w-full h-auto mb-4" />
+            <img src={banner3} alt="banner3Preview" className="w-full h-auto mb-4" />
           </div>
         </div>
 
         <button
           type="submit"
-          className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition"
+          className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition w-full"
         >
           Update UI
         </button>
